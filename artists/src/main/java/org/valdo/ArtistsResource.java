37b8a522af558c2fb7f04ec150a80528b86fb92b
valdo ndimbiarisoa <valdotsiarohasina@gmail.com>
2024-04-24 13:45:28 +0000

    add different route to first project

--- artists/src/main/java/org/valdo/ArtistsResource.java
+++ artists/src/main/java/org/valdo/ArtistsResource.java
@@ -2,15 +2,42 @@ package org.valdo;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+import java.util.UUID;
 
-@Path("/hello")
+
+@Path("/artists")
 public class ArtistsResource {
+  
+  private static List<Artist> artists = List.of(
+    new Artist().id(UUID.randomUUID()).firstName("John").lastName("Lennon"),
+    new Artist().id(UUID.randomUUID()).firstName("Paul").lastName("McCartney"),
+    new Artist().id(UUID.randomUUID()).firstName("George").lastName("Harrison"),
+    new Artist().id(UUID.randomUUID()).firstName("Ringo").lastName("Starr")
+  );
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getAllArtists() {
+    System.out.println(artists);
+    return Response.ok(artists).build();
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getArtistById(@PathParam("id") UUID id) {
+    return Response.ok(artists.stream().filter(artist -> artist.getId().equals(id)).findFirst()).build();
+  }
+    
+  @GET
+  @Path("/count")
+  @Produces(MediaType.TEXT_PLAIN)
+    public Integer countArtists() {
+    return artists.size();
+  }
 }
